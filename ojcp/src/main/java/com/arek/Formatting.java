a8086c47a83a1afa69bae182249b0cc67b954b16
terrypratchet <arek@example.com>
2014-12-01 21:18:41 +0100

    added number formatting

---
+++ ojcp/src/main/java/com/arek/Formatting.java
@@ -0,0 +1,26 @@
+package com.arek;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class Formatting {
+
+	public static void main(String[] args) {
+		NumberFormat nf = NumberFormat.getInstance();
+		
+		System.out.println("max fraction:" + nf.getMinimumFractionDigits());
+		System.out.println("min fraction:" + nf.getMinimumFractionDigits());
+	
+		try {
+			System.out.println(nf.format(4.5f));
+			System.out.println(nf.parse("2.5"));
+			System.out.println(nf.parse("2.56are"));
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
