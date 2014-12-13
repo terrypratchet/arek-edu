0efc37f387fa3066491d06fa4e62fa391d8db162
terrypratchet <arek@example.com>
2014-12-13 14:29:25 +0100

    flow

---
+++ ojcp/src/main/java/com/arek/flow/Assertion.java
@@ -0,0 +1,12 @@
+package com.arek.flow;
+
+public class Assertion {
+
+	public static void main(String[] args) {
+		System.out.println("start");
+		assert 1>3: new Integer(7);
+		System.out.println("i co dalej??");
+		// 1.4 java -source 1.4 com/Arek.java
+	}
+
+}
