d783ac346c1086a4ad0298f0b371d2e31ee37192
Arkadiusz Toman <arkadiusz.toman@lumesse.com>
2015-03-05 06:59:14 +0100

    stateful bean

---
+++ ejb/myejb/src/main/java/com/arek/FileTransferBean.java
@@ -0,0 +1,59 @@
+package com.arek;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.PostActivate;
+import javax.ejb.PrePassivate;
+import javax.ejb.Remote;
+import javax.ejb.Stateful;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Stateful(name=FileTransferBean.EJB_NAME)
+@Remote(FileTransferCommonBusiness.class)
+public class FileTransferBean implements FileTransferCommonBusiness {
+
+	final static Logger logger = LoggerFactory.getLogger(FileTransferBean.class);
+	
+	/**
+	* Name of the EJB, used in Global JNDI addresses
+	*/
+	public static final String EJB_NAME = "FileTransferEJB";
+	
+	
+	@Override
+	public void mkdir(String directory) throws IllegalStateException {
+		logger.error("mkdir");
+
+	}
+
+	@Override
+	public void cd(String directory) throws IllegalStateException {
+		logger.error("cd " + directory);
+
+	}
+
+	@Override
+	public String pwd() throws IllegalStateException {
+		logger.error("pwd");
+		return null;
+	}
+
+	@PrePassivate
+	@PreDestroy
+	@Override
+	public void disconnect() {
+		logger.error("disconnect");
+
+	}
+
+	@PostConstruct
+	@PostActivate
+	@Override
+	public void connect() throws IllegalStateException {
+		logger.error("connect");
+
+	}
+
+}
---
+++ ejb/myejb/src/main/java/com/arek/FileTransferCommonBusiness.java
@@ -0,0 +1,41 @@
+package com.arek;
+
+public interface FileTransferCommonBusiness {
+	/**
+	* Makes a directory of the specified name
+	*
+	* @throws IllegalStateException If the client connection has not been
+	initialized
+	*/
+	void mkdir(String directory) throws IllegalStateException;
+	/**
+	* Changes into the named directory
+	*
+	* @param directory
+	* @throws IllegalStateException If the client connection has not been
+	initialized
+	*/
+	void cd(String directory) throws IllegalStateException;
+	/**
+	* Obtains the name of the current working directory
+	*
+	* @return
+	* @throws IllegalStateException If the client connection has not been
+	initialized
+	*/
+	String pwd() throws IllegalStateException;
+	/**
+	* Denotes that the client is done using this service; flushes
+	* any pending operations and does all appropriate cleanup. If
+	* already disconnected, this is a no-op.
+	*/
+	void disconnect();
+	/**
+	* Opens the underlying connections to the target FTP Server,
+	* performs any other tasks required before commands may be sent
+	* (ie. login, etc)
+	*
+	* @throws IllegalStateException If already initialized/connected
+	*/
+	void connect() throws IllegalStateException;
+}
