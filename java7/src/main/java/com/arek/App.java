8248b2daf2d4fbccdc4b7db70250335d75fd08d2
Arkadiusz Toman <arkadiusz.toman@lumesse.com>
2015-05-26 06:49:08 +0200

    java7 init

---
+++ java7/src/main/java/com/arek/App.java
@@ -0,0 +1,29 @@
+package com.arek;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    	
+        // Diamond Operator
+        Map<String, List<Long>> trades = new TreeMap <> ();
+    }
+    
+    public void testSwitch(String arg){
+    	switch(arg){
+    	case "Arek":
+    		break;
+    	}
+    }
+    
+    
+}
