821c0d7205e00d7e5fbd46613705a175e20cb94d
Arkadiusz Toman <arkadiusz.toman@lumesse.com>
2016-02-21 13:01:12 +0100

    added controller test

--- spring-demo/src/main/java/demo/web/HomeController.java
+++ spring-demo/src/main/java/demo/web/HomeController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @Controller // could be @Component
-public class HomController {
+@RequestMapping({"/", "/homepage"})
+public class HomeController {
 
-	@RequestMapping(value="/", 	method=GET)
+	@RequestMapping(method=GET)
 	public String home(){
 		return "home"; // view name is home
 		/// WEB-INF /views/home.jsp because of you configuration in WebConfig.viewResolver
