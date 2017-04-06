20540a06a2c0224fd1956c1398b8e1767abf10be
terrypratchet <arek@example.com>
2017-04-06 21:34:26 +0200

    refresh

--- spring-cloud/config-client/src/main/java/com/arek/RateController.java
+++ spring-cloud/config-client/src/main/java/com/arek/RateController.java
@@ -1,11 +1,12 @@
 package com.arek;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@RefreshScope
 @Controller
 public class RateController {
 	
